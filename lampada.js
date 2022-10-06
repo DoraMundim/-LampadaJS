fcffef11aaccdb549506803e1d66b7dd2942e600
Dora Alice <114297651+DoraMundim@users.noreply.github.com>
2022-10-06 15:33:38 -0400

    Add files via upload

--- lampada.js
+++ lampada.js
@@ -2,18 +2,23 @@ const turnOn = document.getElementById ('turnOn');
 const turnOff = document.getElementById ('turnOff');
 const lamp = document.getElementById ('lamp');
 
-function lampOn () {
-    lamp.src = './imagem/ligada.png';
+function isLampBroken () {
+    return lamp.src.indexOf ( 'quebrado' ) > -1
 }
 
+function lampOn () {
+    if ( !isLampBroken () ) {
+    lamp.src = './imagem/ligada.png';
+    }
+}
 function lampOff () {
+    if ( !isLampBroken () ) {
     lamp.src = './imagem/desligado.png';
+    }
 }
 function lampBroken (){
     lamp.src = './imagem/quebrado.png';
 }
-
-
 
 turnOn.addEventListener ('click', lampOn );
 turnOff.addEventListener ('click', lampOff );
